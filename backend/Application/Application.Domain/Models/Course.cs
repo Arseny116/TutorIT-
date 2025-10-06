175b27ffb78c699c1ab164dc23101392ba1ddd8c
Yakov <yaxa499@gmail.com>
2025-10-06 20:47:01 +0300

    Create class Course

---
+++ backend/Application/Application.Domain/Models/Course.cs
@@ -0,0 +1,35 @@
+﻿namespace Application.Domain.Models
+{
+    public class Course
+    {
+        /// <summary>
+        /// Заголовок курса
+        /// </summary>
+        public string? Title { get; }
+
+        /// <summary>
+        /// Описание курса
+        /// </summary>
+        public string? Description { get; }
+
+        /// <summary>
+        /// Оценка курса (по 5-ти бальной шкале)
+        /// </summary>
+        public double? Evaluation { get; }
+
+        /// <summary>
+        /// Отзывы о курсе
+        /// </summary>
+        public string? Reviews { get; }
+
+        /// <summary>
+        /// Кол-во людей записанных на курс
+        /// </summary>
+        public int Subscribe { get; }
+
+        /// <summary>
+        /// Кол-во заданий на курсе
+        /// </summary>
+        public int Tasks { get; }
+    }
+}
