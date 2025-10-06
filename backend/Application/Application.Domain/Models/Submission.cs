4ea1c6bfd375dc6f3a01144ca9ef74aed06e8209
Arseny116 <Olifirov24@gmail.com>
2025-10-06 20:56:26 +0300

    Add Domain's entity for codeexecution

---
+++ backend/Application/Application.Domain/Models/Submission.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain.Models
+{
+    public class CodeSubmission
+    {
+        public int ProblemId { get; }
+        public string Language { get; } = string.Empty;
+        public string SourceCode { get; } = string.Empty;
+        public DateTime SubmittedAt { get; }
+
+    }
+}
