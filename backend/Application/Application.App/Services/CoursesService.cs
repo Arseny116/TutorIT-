b08ddab2d27c0dc6440c717766e8d6bd501c1bfd
Yakov <yaxa499@gmail.com>
2025-10-12 23:14:49 +0300

    Create CoursesService

---
+++ backend/Application/Application.App/Services/CoursesService.cs
@@ -0,0 +1,35 @@
+﻿using Application.Domain.Interface;
+using Application.Domain.Models;
+
+namespace Application.App.Services
+{
+    public class CoursesService
+    {
+        private readonly ICoursesRepository _coursesRepository;
+
+        public CoursesService(ICoursesRepository coursesRepository)
+        {
+            _coursesRepository = coursesRepository;
+        }
+
+        public async Task<List<Course>> CetCourses()
+        {
+            return await _coursesRepository.Get();
+        }
+
+        public async Task<Guid> CreateCourse(Course course)
+        {
+            return await _coursesRepository.Create(course);
+        }
+
+        public async Task<Guid> UpdateCourse(Guid id, string title, string description, int tasks)
+        {
+            return await _coursesRepository.Update(id, title, description, tasks);
+        }
+
+        public async Task<Guid> DeleteCourse(Guid id)
+        {
+            return await _coursesRepository.Delete(id);
+        }
+    }
+}
