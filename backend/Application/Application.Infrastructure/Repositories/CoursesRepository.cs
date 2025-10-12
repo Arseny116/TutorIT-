205d599fd7320c8dc0732fc5b09ad11e9d97ba57
Yakov <yaxa499@gmail.com>
2025-10-12 20:53:39 +0300

    Create CoursesRepository

---
+++ backend/Application/Application.Infrastructure/Repositories/CoursesRepository.cs
@@ -0,0 +1,71 @@
+﻿using Application.Domain.Models;
+using Application.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Infrastructure.Repositories
+{
+    public class CoursesRepository
+    {
+        private CourseDbContext _context;
+
+        public CoursesRepository(CourseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Course>> Get()
+        {
+            var courseEntity = await _context.Courses.AsNoTracking().ToListAsync();
+
+            var courses = new List<Course>();
+            foreach (var entity in courseEntity)
+            {
+                var result = Course.Create(entity.Id, entity.Title, entity.Description, entity.Tasks);
+
+                if (result.IsSuccess)
+                {
+                    courses.Add(result.Value);
+                }
+            }
+
+            return courses;
+        }
+
+        public async Task<Guid> Create(Course course)
+        {
+            var courseEntity = new CourseEntity
+            {
+                Id = course.Id,
+                Title = course.Title,
+                Description = course.Description,
+                Evaluation = course.Evaluation,
+                Reviews = course.Reviews,
+                Subscribe = course.Subscribe,
+                Tasks = course.Tasks
+            };
+
+            await _context.Courses.AddAsync(courseEntity);
+            await _context.SaveChangesAsync();
+
+            return courseEntity.Id;
+        }
+
+        public async Task<Guid> Update(Guid id, string title, string description, int tasks)
+        {
+            await _context.Courses.Where(c => c.Id == id)
+                .ExecuteUpdateAsync(s => s
+                .SetProperty(t => t.Title, t => title)
+                .SetProperty(d => d.Description, d => description)
+                .SetProperty(t => t.Tasks, t => tasks));
+
+            return id;
+        }
+
+        public async Task<Guid> Delete(Guid id)
+        {
+            await _context.Courses.Where(c => c.Id == id).ExecuteDeleteAsync();
+
+            return id;
+        }
+    }
+}
