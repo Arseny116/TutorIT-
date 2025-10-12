4e1ef09ed0f01d27e158f1783fb5261197db0f2c
Yakov <yaxa499@gmail.com>
2025-10-12 20:13:33 +0300

    Create CourseConfiguration

---
+++ backend/Application/Application.Infrastructure/Configuration/CourseConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Application.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Infrastructure.Configuration
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<CourseEntity>
+    {
+        public void Configure(EntityTypeBuilder<CourseEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(t => t.Title).IsRequired();
+
+            builder.Property(d => d.Description).IsRequired();
+
+            builder.Property(t => t.Tasks).IsRequired();
+        }
+    }
+}
