0bd406be905fa507aaedc814509c85adaadf03fa
Yakov <yaxa499@gmail.com>
2025-10-19 18:17:32 +0300

    Create IAutorsRepository

---
+++ backend/Application/Application.Domain/Interface/IAutorsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Domain.Models;
+
+namespace Application.Domain.Interface
+{
+    public interface IAutorsRepository
+    {
+        Task<List<Autor>> Get();
+
+        Task<Guid> Create(Autor autor);
+
+        Task<Guid> Update(Guid id, string name, string description);
+
+        Task<Guid> Delete(Guid id);
+    }
+}
