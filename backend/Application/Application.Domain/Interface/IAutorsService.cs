421b48f6708ef994201213f3b3d8267c8522a71e
Yakov <yaxa499@gmail.com>
2025-10-19 18:42:21 +0300

    Create IAutorsService

---
+++ backend/Application/Application.Domain/Interface/IAutorsService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Domain.Models;
+
+namespace Application.Domain.Interface
+{
+    public interface IAutorsService
+    {
+        Task<List<Autor>> CetAutors();
+
+        Task<Guid> CreateAutor(Autor autor);
+
+        Task<Guid> UpdateAutor(Guid id, string name, string description);
+
+        Task<Guid> DeleteAutor(Guid autorId);
+    }
+}
