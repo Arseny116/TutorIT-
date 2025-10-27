0726e008c45a72f141bdddfe0f59fc911e9c1f62
Yakov <yaxa499@gmail.com>
2025-10-27 19:24:01 +0300

    Create class TaskCreator

---
+++ backend/Application/Application.Domain/Models/TaskCreator.cs
@@ -0,0 +1,50 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Application.Domain.Models
+{
+    public class TaskCreator
+    {
+        /// <summary>
+        /// Id Задачи
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Name { get; } = string.Empty;
+
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Description { get; } = string.Empty;
+
+        /// <summary>
+        /// Список вопросов
+        /// </summary>
+        public List<Question> Questions { get; } = [];
+
+        public TaskCreator(Guid id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public static Result<TaskCreator> Create(Guid id, string name, string description)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Result.Failure<TaskCreator>("Имя задачи не может быть пустым");
+            }
+            if (string.IsNullOrEmpty(description))
+            {
+                return Result.Failure<TaskCreator>("Описание задачи не может быть пустым");
+            }
+
+            TaskCreator taskCreator = new TaskCreator(id, name, description);
+
+            return Result.Success(taskCreator);
+        }
+    }
+}
