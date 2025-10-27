af62b42ea7877cb5a2617a081a91e1bc607c8c49
Yakov <yaxa499@gmail.com>
2025-10-27 16:44:56 +0300

    Create class Question

---
+++ backend/Application/Application.Domain/Models/Question.cs
@@ -0,0 +1,41 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Application.Domain.Models
+{
+    public class Question
+    {
+        /// <summary>
+        /// Id вопроса
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Сам вопроса
+        /// </summary>
+        public string Name { get; } = string.Empty;
+
+        /// <summary>
+        /// Ответ на вопрос
+        /// </summary>
+        public bool Answer { get; }
+
+        public Question(Guid id, string name, bool answer)
+        {
+            Id = id;
+            Name = name;
+            Answer = answer;
+        }
+
+        public Result<Question> Create(Guid id, string name, bool answer)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Result.Failure<Question>("Вопрос не может путь пустым");
+            }
+
+            Question question = new Question(id, name, answer);
+
+            return Result.Success(question);
+        }
+    }
+}
