889927c646e60bd75b931057e33958e49b7d30e9
Yakov <yaxa499@gmail.com>
2025-10-27 21:17:01 +0300

    Create TaskCreatorEntity & Edit QuestionEntity

---
+++ backend/Application/Application.Infrastructure/Entities/TaskCreatorEntity.cs
@@ -0,0 +1,27 @@
+﻿using Application.Domain.Models;
+
+namespace Application.Infrastructure.Entities
+{
+    public class TaskCreatorEntity
+    {
+        /// <summary>
+        /// Id Задачи
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Список вопросов
+        /// </summary>
+        public List<Question> Questions { get; set; } = [];
+    }
+}
