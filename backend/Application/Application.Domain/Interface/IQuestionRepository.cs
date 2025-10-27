eb13d19081733536319828f023edc4ce5580c40b
Yakov <yaxa499@gmail.com>
2025-10-27 17:18:09 +0300

    Create IQuestionRepository

---
+++ backend/Application/Application.Domain/Interface/IQuestionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Domain.Models;
+
+namespace Application.Domain.Interface
+{
+    public interface IQuestionRepository
+    {
+        Task<List<Question>> Get();
+
+        Task<Guid> Create(Question question);
+
+        Task<Guid> Update(Guid id, string name, bool answer);
+
+        Task<Guid> Delete(Guid id);
+    }
+}
