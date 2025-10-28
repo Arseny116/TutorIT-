2c53ef96a09ca52788ded5a103b2a85258aa3b44
Yakov <yaxa499@gmail.com>
2025-10-28 14:54:47 +0300

    Create TasksCreatorRequest&Response

---
+++ backend/Application/Application.API/DTO/TasksCreator/TasksCreatorRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.API.DTO.TasksCreator
+{
+    public record class TasksCreatorRequest(
+        string name,
+        string description);
+}
---
+++ backend/Application/Application.API/DTO/TasksCreator/TasksCreatorResponse.cs
@@ -0,0 +1,10 @@
+﻿using Application.Domain.Models;
+
+namespace Application.API.DTO.TasksCreator
+{
+    public record class TasksCreatorResponse(
+        Guid id,
+        string? name,
+        string? description,
+        List<Question> questions);
+}
