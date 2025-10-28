2b47bcbb86a74b81bdf2981c3dc531d2449d3903
Yakov <yaxa499@gmail.com>
2025-10-28 14:30:17 +0300

    Create ITasksCreatorService

---
+++ backend/Application/Application.Domain/Interface/ITasksCreatorService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Domain.Models;
+
+namespace Application.Domain.Interface
+{
+    public interface ITasksCreatorService
+    {
+        Task<List<TaskCreator>> GetTasksCreators();
+
+        Task<Guid> CreateTaskCreator(TaskCreator taskCreator);
+
+        Task<Guid> UpdateTaskCreator(Guid id, string name, string description);
+
+        Task<Guid> DeleteTaskCreator(Guid taskId);
+    }
+}
