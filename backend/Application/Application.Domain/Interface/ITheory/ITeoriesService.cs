9ce74368ce4ac72d1b64cc48213bf379efb904da
Yakov <yaxa499@gmail.com>
2025-11-03 16:37:39 +0300

    Create ITeoriesService

---
+++ backend/Application/Application.Domain/Interface/ITheory/ITeoriesService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Domain.Models;
+
+namespace Application.Domain.Interface.ITheory
+{
+    public interface ITeoriesService
+    {
+        Task<List<Theory>> GetTheories();
+
+        Task<Guid> CreateTheory(Theory theory);
+
+        Task<Guid> UpdateTheory(Guid id, string name, string article);
+
+        Task<Guid> DeleteTheory(Guid id);
+    }
+}
