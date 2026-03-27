94bb659d10d4a8df100f972e4e0cb3a95beaef23
Yakov <yaxa499@gmail.com>
2026-03-27 18:04:21 +0300

    feat: add email validation

--- backend/ApplicationUsers/ApplicationUsers.App/Handlers/RegisterUserHandler.cs
+++ backend/ApplicationUsers/ApplicationUsers.App/Handlers/RegisterUserHandler.cs
@@ -1,22 +1,26 @@
 using ApplicationUsers.App.Command;
-using ApplicationUsers.Infrastructure;
-using MediatR;
 using ApplicationUsers.Domain;
+using ApplicationUsers.Domain.Interface;
+using ApplicationUsers.Infrastructure;
 using CSharpFunctionalExtensions;
+using MediatR;
 using Microsoft.Extensions.Logging;
+
 namespace ApplicationUsers.App.Handlers
 {
     public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, Result<Guid>>
     {
         private readonly IPasswordHasher _hasher;
         private readonly IUserRepository _userRepository;
+        private readonly IMailService _mailService;
         private readonly ILogger _logger;
 
-        public RegisterUserHandler(ILogger<RegisterUserHandler> logger,  IPasswordHasher hasher, IUserRepository userRepository)
+        public RegisterUserHandler(ILogger<RegisterUserHandler> logger,  IPasswordHasher hasher, IUserRepository userRepository, IMailService mailService)
         {
             _logger = logger;
             _hasher = hasher;
             _userRepository = userRepository;
+            _mailService = mailService;
         }
     
         public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -27,6 +31,15 @@ namespace ApplicationUsers.App.Handlers
             if (user.IsFailure)
             {
                 return Result.Failure<Guid>(user.Error);
+            }
+
+
+            var mailData = new MailData(request.Email, "Добро пожаловать в TutorIt!");
+            var isMailValid = await _mailService.SendHelloAsync(mailData);
+
+            if (!isMailValid)
+            {
+                return Result.Failure<Guid>("Указанный почтовый домен не существует или недоступен.");
             }
 
             var user_guid = await _userRepository.CreateUser(user.Value, cancellationToken);
